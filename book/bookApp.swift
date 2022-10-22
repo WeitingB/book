03f414354465a1e495769721230b26979f70437a
Weitingbai <weitingbai408@gmail.com>
2022-10-22 13:15:19 +0800

    Initial Commit

---
+++ book/bookApp.swift
@@ -0,0 +1,17 @@
+//
+//  bookApp.swift
+//  book
+//
+//  Created by Caren Chin on 2022/10/22.
+//
+
+import SwiftUI
+
+@main
+struct bookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
